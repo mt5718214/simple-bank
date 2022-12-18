af1dbb4d1abb8c1e1a2cee66a72cb75aa00bd791
tsungilin <mt5718214@gmail.com>
2022-12-18 16:51:13 +0800

    add db transaction func & transfer test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,14 +15,16 @@ const (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDrive, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDrive, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err.Error())
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
