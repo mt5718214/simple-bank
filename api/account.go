71616098ee39820565f43564fb72258970d2d2d3
tsungilin <mt5718214@gmail.com>
2022-12-21 20:43:06 +0800

    add getAccount endpoint

--- api/account.go
+++ api/account.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 	db "simplebank/db/sqlc"
 
@@ -26,6 +27,31 @@ func (server *Server) createAccount(c *gin.Context) {
 	}
 	account, err := server.store.CreateAccount(c, arg)
 	if err != nil {
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	c.JSON(http.StatusOK, account)
+}
+
+type getAccountReq struct {
+	ID int64 `uri:"id" binding:"required, min=1"`
+}
+
+func (server *Server) getAccount(c *gin.Context) {
+	var req getAccountReq
+	if err := c.ShouldBindUri(&req); err != nil {
+		c.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(c, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
 		c.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
