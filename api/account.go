bd124100bfe0de67dad4b73674f2869c7ab3feb8
tsungilin <mt5718214@gmail.com>
2022-12-24 22:14:25 +0800

    fix typo

--- api/account.go
+++ api/account.go
@@ -35,7 +35,7 @@ func (server *Server) createAccount(c *gin.Context) {
 }
 
 type getAccountReq struct {
-	ID int64 `uri:"id" binding:"required, min=1"`
+	ID int64 `uri:"id" binding:"required,min=1"`
 }
 
 func (server *Server) getAccount(c *gin.Context) {
@@ -60,8 +60,8 @@ func (server *Server) getAccount(c *gin.Context) {
 }
 
 type listAccountReq struct {
-	PageID   int32 `form:"page_id" binding:"required, min=1"`
-	PageSize int32 `form:"page_size" binding:"required, min=5 max=10"`
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
 func (server *Server) listAccount(c *gin.Context) {
