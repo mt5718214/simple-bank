50e2f9d1c49c5e3c37af37877bad0c520e95ccb4
tsungilin <mt5718214@gmail.com>
2023-07-30 18:16:06 +0800

    fix: Fix wrong variable

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -93,7 +93,7 @@ func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (Tr
 		if arg.FromAccountID > arg.ToAccountID {
 			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
 		} else {
-			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
 		}
 
 		return err
