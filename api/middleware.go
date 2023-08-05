202e235ec3aee2146a62aeb6806ef4b72a8670f8
tsungilin <mt5718214@gmail.com>
2023-08-05 20:53:18 +0800

    feat: Add auth middleware

---
+++ api/middleware.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"simplebank/token"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	authPayloadKey = "auth_payload_key"
+	authTypeBearer = "Bearer"
+)
+
+func authMiddleware(tokenMaker *token.PasetoMaker) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authorizationHeader := ctx.GetHeader("authorization")
+		if len(authorizationHeader) == 0 {
+			err := errors.New("authorization header is not provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		fields := strings.Fields(authorizationHeader)
+		if len(fields) < 2 {
+			err := errors.New("invalid authorization header format")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		authType := fields[0]
+		if authType != authTypeBearer {
+			err := fmt.Errorf("unsupposed authorization type %s", authType)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		accessToken := fields[1]
+		payload, err := tokenMaker.VerifyToken(accessToken)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		ctx.Set(authPayloadKey, payload)
+		ctx.Next()
+	}
+}
