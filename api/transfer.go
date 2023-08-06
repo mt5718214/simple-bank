bd2267cac723b6ecd0e24e25893d26f32a2cb7e5
tsungilin <mt5718214@gmail.com>
2023-08-06 17:24:34 +0800

    refactor: Take user info from authPayload

--- api/transfer.go
+++ api/transfer.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"net/http"
 	db "simplebank/db/sqlc"
+	"simplebank/token"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,11 +25,20 @@ func (server *Server) createTransfer(c *gin.Context) {
 		return
 	}
 
-	if !server.validAccount(c, req.FromAccountID, req.Currency) {
+	fromAccount, valid := server.validAccount(c, req.FromAccountID, req.Currency)
+	if !valid {
 		return
 	}
 
-	if !server.validAccount(c, req.FromAccountID, req.Currency) {
+	authPayload := c.MustGet(authPayloadKey).(*token.Payload)
+	if fromAccount.Owner != authPayload.Username {
+		err := errors.New("from account doesn't belong to authenticated user")
+		c.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	_, valid = server.validAccount(c, req.FromAccountID, req.Currency)
+	if !valid {
 		return
 	}
 
@@ -45,23 +56,23 @@ func (server *Server) createTransfer(c *gin.Context) {
 	c.JSON(http.StatusOK, result)
 }
 
-func (server *Server) validAccount(c *gin.Context, accountID int64, currency string) bool {
+func (server *Server) validAccount(c *gin.Context, accountID int64, currency string) (db.Account, bool) {
 	account, err := server.store.GetAccount(c, accountID)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			c.JSON(http.StatusNotFound, errorResponse(err))
-			return false
+			return account, false
 		}
 
 		c.JSON(http.StatusInternalServerError, errorResponse(err))
-		return false
+		return account, false
 	}
 
 	if account.Currency != currency {
 		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", accountID, account.Currency, currency)
 		c.JSON(http.StatusBadRequest, errorResponse(err))
-		return false
+		return account, false
 	}
 
-	return true
+	return account, true
 }
